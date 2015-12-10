7ce6f681d23a12e52170ac577d36f2ac32d75e4c
diedona <diedona@gmail.com>
2015-12-10 00:23:56 -0200

    PDF

--- DDona.Relatorios.Web/Controllers/ReportController.cs
+++ DDona.Relatorios.Web/Controllers/ReportController.cs
@@ -1,6 +1,9 @@
 ﻿using DDona.Relatorios.Web.LojaReference;
+using DDona.Relatorios.Web.ViewModel.PDF;
+using Rotativa;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +24,48 @@ namespace DDona.Relatorios.Web.Controllers
             {
                 return Json(LojaClient.GetRelatorioPaginadoDatatables(DatatablesModel));
             }
+        }
+
+        public ActionResult Report2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Report2(int QtdItens)
+        {
+            Debug.WriteLine("{0} - Comecei a gerar dados", DateTime.Now);
+            List<PdfFakeResult> Data = GenerateFakeData(QtdItens);
+            Debug.WriteLine("{0} - Terminei de gerar dados", DateTime.Now);
+            ViewAsPdf Pdf = new ViewAsPdf("Report2PartialPdf", Data);
+            Pdf.FileName = "Report2.pdf";
+            Debug.WriteLine("{0} - Retornando o Download", DateTime.Now);
+
+            return Pdf;
+        }
+
+        private List<PdfFakeResult> GenerateFakeData(int QtdItens)
+        {
+            List<PdfFakeResult> Data = new List<PdfFakeResult>(QtdItens);
+
+            for (int i = 0; i < QtdItens; i++)
+            {
+                Data.Add(GenerateFakeDataItem(i));
+            }
+
+            return Data;
+        }
+
+        private PdfFakeResult GenerateFakeDataItem(int Index)
+        {
+            return new PdfFakeResult()
+            {
+                DataNascimento = new DateTime(2000,12,15),
+                Estado = "SP",
+                GrauInvestimento = "AAA+",
+                Nome = "José Almeida",
+                Saldo = 1500.5m
+            };
         }
     }
 }
---
+++ DDona.Relatorios.Web/ViewModel/PDF/PdfFakeResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDona.Relatorios.Web.ViewModel.PDF
+{
+    public class PdfFakeResult
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Estado { get; set; }
+        public string GrauInvestimento { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
